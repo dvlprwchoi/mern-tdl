abddb08895eb202e201b3185155e4d8e3562c43c
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-23 00:54:53 -0600

    Add separate css files

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,5 +1,20 @@
+import './Welcome.css';
+
 function Welcome() {
-  return <div className="welcome">This is welcome page</div>;
+  return (
+    <div className="welcome">
+      <div className="welcome-message">
+        <h2>Welcome to MERN Todo List application!</h2>
+        <p>
+          You can Log In or Sign Up for an acount to use MERN Todo List app.
+        </p>
+      </div>
+      <div className="welcome-buttons">
+        <button className="login-button">Log In</button>
+        <button className="signup-button">Sign Up</button>
+      </div>
+    </div>
+  );
 }
 
 export default Welcome;
