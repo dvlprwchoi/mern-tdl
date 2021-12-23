4ad22682457cc536e686f0c80e43f01beb5013f3
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-23 00:13:49 -0600

    Add favicon and Update css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,11 @@
 function Header() {
   return (
     <div className="header">
-      <div className="header-title">MERN Todo List</div>
+      <div className="header-title">
+        <img className="header-app-logo" src="./app-logo.png" alt="App Logo" />
+        <h1 className="header-title-text-mern">MERN</h1>
+        <h1 className="header-title-text-tdl">Todo List</h1>
+      </div>
     </div>
   );
 }
