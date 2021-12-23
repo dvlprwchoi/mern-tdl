14eaeb78530d8c02c0fff13d3f343084e2c9fd66
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-23 01:30:03 -0600

    Install react router dome v5.0 and Add link and router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <Main />
+            <Footer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function Header() {
   return (
     <div className="header">
       <div className="header-title">
-        <img className="header-app-logo" src="./app-logo.png" alt="App Logo" />
+        <Link to="/">
+          <img
+            className="header-app-logo"
+            src="./app-logo.png"
+            alt="App Logo"
+          />
+        </Link>
         <h1 className="header-title-text-mern">MERN</h1>
         <h1 className="header-title-text-tdl">Todo List</h1>
       </div>
