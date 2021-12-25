0a4a966509a10ac39e89873fd00110577a213efd
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-24 19:00:04 -0600

    Add add new todo form

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -9,14 +9,48 @@ function Dashboard({ username }) {
           <h2>Good morning, {username}!</h2>
           <p>You can add, edit, delete, clear</p>
         </div>
+        <div className="todo-list-container">
+          <div className="todo-list-board">
+            <li>AAA</li>
+            <li>BBB</li>
+            <li>CCC</li>
+            <li>CCC</li>
+            {/* <li>CCC</li>
+            <li>CCC</li>
+            <li>CCC</li>
+            <li>CCC</li>
+            <li>CCC</li>
+            <li>CCC</li>
+            <li>CCC</li>
+            <li>CCC</li> */}
+          </div>
+          <div className="add-todo-list-div">
+            <form className="add-todo-list-form">
+              <label className="add-todo-list-input-label" htmlFor="todo">
+                Todo:
+              </label>
+              <input
+                className="add-todo-list-input-box"
+                type="text"
+                placeholder="Type Todo"
+                name="todo"
+                id="todo"
+                autoFocus
+                required
+              />
+              <button className="add-button button-small">Add</button>
+            </form>
+          </div>
+        </div>
         <div className="dashboard-buttons-div buttons-div">
+          {/* <button className="edit-button button">Edit</button> */}
+          {/* <button className="delete-button button">Delete</button> */}
+          <button className="clear-button button">
+            Clear Completed Todo(s)
+          </button>
           <Link to="/logout">
             <button className="logout-button button">Log Out</button>
           </Link>
-          <button className="add-button button">Add</button>
-          <button className="edit-button button">Edit</button>
-          <button className="delete-button button">Delete</button>
-          <button className="clear-button button">Clear</button>
         </div>
       </div>
     </div>
