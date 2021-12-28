ff589dbda8e5094cfe38b09bc8d9d5497281e9cb
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-12-28 07:22:47 -0600

    Add login and error handle function and Contect to backend

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,48 @@
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react/cjs/react.development';
+import { CredentialsContext } from '../../App';
 import './Login.css';
 
+import { _error } from '../Signup/Signup';
+
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useContext(CredentialsContext);
+
+  const _login = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:4000/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+      .then(_error)
+      .then(() => {
+        setCredentials({ username, password });
+        navigate('/dashboard');
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
   return (
     <div className="main">
       <div className="login">
         <div className="login-message">
           <h2>Log In Page</h2>
+          {!!error && <span style={{ color: 'red' }}>{error}</span>}
         </div>
         <div className="login-information">
-          <form className="login-information-form">
+          <form className="login-information-form" onSubmit={_login}>
             <div className="login-username-div">
               <label className="login-username-input-label" htmlFor="username">
                 Username:
@@ -21,6 +55,9 @@ function Login() {
                 id="username"
                 autoFocus
                 required
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
               />
             </div>
             <div className="login-password-div">
@@ -34,6 +71,9 @@ function Login() {
                 name="password"
                 id="password"
                 required
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
               />
             </div>
             <div className="login-button-div buttons-div">
